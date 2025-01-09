6f6d8a17cf1f006887e954b58fca9a3733ff9e16
szymon27 <szymonsamsel27@gmail.com>
2025-01-09 17:34:21 +0100

    add bfs implementation with tests

--- pkgs/bfs/bfs.py
+++ pkgs/bfs/bfs.py
@@ -1,8 +1,38 @@
 from pkgs.data.graph import Graph
 
-class BFS():
+class BFS:
     def __init__(self, graph: Graph):
         self.graph = graph
         self.visited = []
         self.queue = []
-        self.result = []
+        self.visited_edges = []
+
+    def bfs(self, start_node):
+        if start_node not in self.graph.nodes:
+            raise ValueError("The start node is not in the graph")
+
+        self.visited = []
+        self.queue = [start_node]
+        self.visited_edges = []
+
+        while self.queue:
+            current_node = self.queue.pop(0)
+
+            if current_node not in self.visited:
+                self.visited.append(current_node)
+
+                for edge in self.graph.edges:
+                    neighbor = None
+                    if edge[0] == current_node and edge[1] not in self.visited:
+                        neighbor = edge[1]
+                    elif edge[1] == current_node and edge[0] not in self.visited:
+                        neighbor = edge[0]
+
+                    if neighbor and neighbor not in self.queue:
+                        self.queue.append(neighbor)
+                        self.visited_edges.append(edge)
+
+        return {
+            "visited_nodes": self.visited,
+            "visited_edges": self.visited_edges
+        }
