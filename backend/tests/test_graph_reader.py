d18da653e1f4437d9eb7e9356f45adc65c7124cb
wiktorwp <wiktorplu@gmail.com>
2025-01-18 17:35:40 +0100

    Fix file paths

--- backend/tests/test_graph_reader.py
+++ backend/tests/test_graph_reader.py
@@ -5,14 +5,14 @@ from pkgs.helpers.graph_reader import read_graph_from_txt_file, read_lines
 
 class TestGraphReader(unittest.TestCase):
     def test_graph_reader(self):
-        graph = read_graph_from_txt_file("backend/tests/data/road-test-CA.txt")
+        graph = read_graph_from_txt_file("tests/data/road-test-CA.txt")
 
         assert isinstance(graph, Graph)
         assert len(graph.nodes) == 945
         assert len(graph.edges) == 2081
 
     def test_read_lines(self):
-        graph = read_lines("backend/tests/data/road-test-CA.txt")
+        graph = read_lines("tests/data/road-test-CA.txt")
 
         assert len(graph) == 2081
 
